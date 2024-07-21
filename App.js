6c64d841e8cdd778339a3e22b819dfe1bc22e46e
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2024-07-21 14:01:51 +0530

    complex html structure in react using react-cdn link

--- App.js
+++ App.js
@@ -1,5 +1,28 @@
+// React Element is normal javascript object
 
-    const heading = React.createElement("h1",{id:"heading"},"Hello world from React!");
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(heading);
+
+/* <div id="parent">
+    <div id="child">
+<h1>I m h1 tag</h1>
+    </div>
+
+</div> */
+
+
+const parent = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+
+       [ React.createElement("h1",{},"H1 TAG"),React.createElement("h2",{},"H2 TAG")]
+    ),
+    React.createElement("div",{id:"child2"},
+
+        [ React.createElement("h1",{},"H1 TAG"),React.createElement("h2",{},"H2 TAG")]
+     )
+)
+
+
+
+    // const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello world from React!");
+     const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(parent);/// render method of convert the object to html.
 
