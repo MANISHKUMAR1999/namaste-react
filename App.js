0a7f9677e457b039ea9bc815c15b26a126a13bb6
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2024-07-25 14:11:50 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 // React Element is normal javascript object
 
-
 /* <div id="parent">
     <div id="child">
 <h1>I m h1 tag</h1>
@@ -8,21 +7,42 @@
 
 </div> */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+// React Functional with kfuntion keyboard Component
+
+const elem = (
+    <h1>Hello eleme </h1>
+   
+)
+const TitleComponent = function() {
+   return ( 
+    <div>
+        {elem}
+        hello title
+    </div>
+)
+}
+
+//React Functional with kfuntion keyboard
+
+const number = 10000
+
+const HeadingComponent = () => 
+    (
+
+   
+  <div id="container">
+    {TitleComponent()}
+    <h2>{number}</h2>
+    <TitleComponent/>
+    
 
 
+    <h1>Hello world</h1>
+  </div>
+   )
 
-
-
-// Jsx is a hmtl like syntax
-// JSX(transpiled before it reaches the js) -> PARCEL - Babel
-
-// JSX => React.createElement => ReactElement - JS Object => HTMLElement(render)
-    const Jsxheading = (<h1  
-        className='head'>Namaste React Using Jsx
-         </h1>)
-
-     const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(Jsxheading);/// render method of convert the object to html.
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeadingComponent/>)
